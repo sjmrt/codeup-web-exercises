0bd6fae349298fd8f85e4530949cd06f423563c2
Sj <sjmrstevens@gmail.com>
2015-11-04 15:23:59 -0600

    added prompts for PHP fizzbuzz to change increment, start, and end points

--- public/php/fizzbuzz.php
+++ public/php/fizzbuzz.php
@@ -1,8 +1,18 @@
 <?php
 
-for ($number = 1; $number <= 100; $number++)
+fwrite(STDOUT, 'Please pick a starting number ');
+$start = fgets(STDIN);
+
+fwrite(STDOUT, 'Please pick an ending number ');
+$end = fgets(STDIN);
+
+fwrite(STDOUT, 'Please pick an incrementer ');
+$increment = fgets(STDIN);
+
+
+for ($number = $start ; $number <= $end; $number = $number + $increment)
 {
-    if ($number % 3 == 0 && $number % 5 ==0) {
+    if ($number % 3 == 0 && $number % 5 == 0) {
         echo "FizzBuzz" . PHP_EOL;
     }
     else if ($number % 3 == 0) {
