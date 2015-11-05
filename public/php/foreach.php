0120cef8c2d84ad9e122368bb4592f42bc2e7aac
Sj <sjmrstevens@gmail.com>
2015-11-05 09:38:57 -0600

    created PHP for each to check for scalar values

--- public/php/foreach.php
+++ public/php/foreach.php
@@ -11,17 +11,7 @@ $things = array(
 	(string) 11);
 
 foreach($things as $value){
-	if (is_integer($value)){
-		var_dump("{$value} is an integer." . PHP_EOL);
-	} else if (is_float($value)){
-		var_dump("{$value} is a float." . PHP_EOL);
-	} else if (is_bool($value)){
-		var_dump("{$value} is boolean." . PHP_EOL);
-	} else if (is_array($value)){
+	if (is_scalar($value)){
 		var_dump($value);
-	} else if (is_null($value)){
-		var_dump("{$value} is null." . PHP_EOL);
-	} else if (is_string($value)){
-		var_dump("{$value} is a string." . PHP_EOL);
 	}
 }
