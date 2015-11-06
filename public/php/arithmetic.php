29459a1f818957d119da6b608162db3561ee1657
Sj <sjmrstevens@gmail.com>
2015-11-06 10:13:57 -0600

    updated arithmetic php to check for numeric values

--- public/php/arithmetic.php
+++ public/php/arithmetic.php
@@ -2,48 +2,85 @@
 
 function add($a, $b)
 {
-    return $a + $b;
+	if(is_numeric($a) && is_numeric($b))
+	{
+    	return $a + $b;
+    } else 
+    {
+    	return "Error! Both arguments must be numbers!";
+    }
 }
 
 function subtract($a, $b)
 {
-	return $a - $b;
+	if(is_numeric($a) && is_numeric($b))
+	{
+		return $a - $b;
+	} else 
+    {
+    	return "Error! Both arguments must be numbers!";
+    }
 }
 
 function multiply($a, $b)
 {
-	return $a*$b;	
+	if(is_numeric($a) && is_numeric($b))
+	{
+		return $a*$b;
+	} else 
+    {
+    	return "Error! Both arguments must be numbers!";
+    }	
 }
 
 function divide($a, $b)
 {
-	return $a/$b;
+	if(((is_numeric($a) && is_numeric($b))) && ($a != 0 && $b !=0))
+	{
+		return $a/$b;
+	} else 
+    {
+    	return "Error! Both arguments must be numbers not equal to zero!";
+    }
 }
 
 function modulus($a, $b)
 {
-	return $a%$b;
+	if(is_numeric($a) && is_numeric($b))
+	{
+		return $a%$b;
+	} else 
+    {
+    	return "Error! Both arguments must be numbers!";
+    }
 }
 
 //solve multiply function using add function and for loop
-function addtwo($a, $b){
-	$sum = 0;
-	for($i = 0 ; $i<$b; $i++){
-		$sum = $sum + $a;
-	}
-	return $sum;
+function addtwo($a, $b)
+{
+	if(is_numeric($a) && is_numeric($b))
+	{
+		$sum = 0;
+		for($i = 0 ; $i<$b; $i++){
+			$sum = $sum + $a;
+		}
+		return $sum;
+	} else 
+    {
+    	return "Error! Both arguments must be numbers!";
+    }
 }
 
-echo add(3,5) . PHP_EOL;
+echo add(6,3) . PHP_EOL;
 
-echo subtract(5,1) . PHP_EOL;
+echo subtract(12,3) . PHP_EOL;
 
-echo multiply(3,5) . PHP_EOL;
+echo multiply(3,3) . PHP_EOL;
 
-echo divide(4,1) . PHP_EOL;
+echo divide(18,2) . PHP_EOL;
 
-echo modulus(3,2) . PHP_EOL;
+echo modulus(19,10) . PHP_EOL;
 
-echo addtwo(4,5) . PHP_EOL;
+echo addtwo(3,3) . PHP_EOL;
 
 ?>
