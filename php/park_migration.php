4d27e97e543f37c9c331094b1827bc25c9e8d446
Sj <sjmrstevens@gmail.com>
2015-12-03 20:58:29 -0600

    updated migration file to include url column in national parks table

--- php/park_migration.php
+++ php/park_migration.php
@@ -12,9 +12,11 @@ $dbc->exec($drop);
 $create = 'CREATE TABLE national_parks (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     name VARCHAR(240) NOT NULL,
+    url VARCHAR(240) NOT NULL,
     location VARCHAR(50) NOT NULL,
     date_established DATE NOT NULL,
     area_in_acres DOUBLE NOT NULL,
+    description TEXT NOT NULL,
     PRIMARY KEY (id)
 )';
 
